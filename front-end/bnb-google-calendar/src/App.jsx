237146adc9ba7af3d4ca971de6df6fe6d7cc6445
Bryce Young <byoung006@gmail.com>
2022-11-22 20:27:55 -0800

    Made a few changes to rendering saved Dates. Formatted Dates. Added Mui components.

--- front-end/bnb-google-calendar/src/App.jsx
+++ front-end/bnb-google-calendar/src/App.jsx
@@ -1,7 +1,31 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { Grid } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Grid,
+  TextField,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { color, styled } from "@mui/system";
+
+const StyledButton = styled("button")({
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  border: "1px solid #FF8E53",
+  borderRadius: "3px",
+  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  color: "black",
+  height: "48px",
+  padding: "0 30px",
+  // add color to the text of the button
+  "&:hover": {
+    background: "linear-gradient(45deg, #FE6B8B 10%, #FF8E53 50%)",
+  },
+});
+
 const EventInput = ({
   placeholder,
   label,
@@ -23,13 +47,16 @@ const EventInput = ({
           </span>
         ))}
         <br />
-        <input
-          className={"box"}
+        <TextField
           type={type}
           onChange={onChange}
           placeholder={placeholder}
+          style={{
+            background: "#fff",
+            color: "black",
+          }}
           key={phrase.id}
-          {...(type !== "file" ? { value: events[name] } : {})}
+          {...{ value: events[name] }}
           name={name}
         />
       </label>
@@ -48,6 +75,7 @@ const EventInput = ({
           form={events}
           {...{ value: events[name] }}
           onChange={onChange}
+          style={{ color: "red" }}
           key={phrase.id}
           name={name}
         />
@@ -57,45 +85,60 @@ const EventInput = ({
 };
 
 const BookedDates = ({ bookedDates }) => {
-  console.log(bookedDates, "bookedDates");
   return (
     <Grid container spacing={3}>
-      {bookedDates.map((bookedDate) => {
-        return (
-          <Grid item xs={6}>
-            <p>{bookedDate.description}</p>
-            <p>{bookedDate.summary}</p>
-            <p>{bookedDate.start.date}</p>
-            <p>{bookedDate.end.date}</p>
-            <p>{bookedDate.attendees}</p>
-          </Grid>
-        );
-      })}
+      <Grid item xs={6}>
+        <p>{bookedDates.description}</p>
+        <p>{bookedDates.summary}</p>
+        <p>{bookedDates.start.date}</p>
+        <p>{bookedDates.end.date}</p>
+        <p>{bookedDates.attendees}</p>
+      </Grid>
     </Grid>
   );
 };
+
 function App() {
   const [events, setEvents] = useState({});
   const [savedEvents, setSavedEvents] = useState([]);
-  console.log(savedEvents, "saved events");
+  console.log(savedEvents, "savedEvents");
   const [eventDataErrors, setEventDataErrors] = useState({});
+  const [isBooked, setIsBooked] = useState(false);
+  const [bookedDates, setBookedDates] = useState({});
+
   const handleInputChange = (e) => {
     setEvents({ ...events, [e.target.name]: e.target.value });
   };
+
   function getBackendApi() {
     let host = window.location.host;
     if (host === "localhost:5173") {
       return "http://localhost:3000";
     }
   }
+
+  const handleDateFormat = ({ savedEvents }) => {
+    //format date to Month Day, Year format
+    savedEvents.map((event) => {
+      let startAndEndDates = {
+        start: new Date(event.start.date).toDateString(),
+        end: new Date(event.end.date).toDateString(),
+      };
+      setBookedDates(startAndEndDates);
+      return startAndEndDates;
+    });
+  };
+
+  const init = async () => {
+    const { data } = await axios.get(`${getBackendApi()}/`);
+    console.log(data, "data");
+    setSavedEvents(data.events);
+    handleDateFormat({ savedEvents: data.events });
+  };
+
   useEffect(() => {
-    const init = async () => {
-      const { data } = await axios.get(`${getBackendApi()}/`);
-      console.log(data, "data");
-      setSavedEvents(data.events);
-    };
     init();
-  }, []);
+  }, [isBooked]);
 
   const handleAddEvent = (e) => {
     console.log(e, "e.target.value");
@@ -112,6 +155,7 @@ function App() {
         console.log(response, "response");
         if (response.status === 200) {
           console.log(response.status, "success");
+          setIsBooked(true);
         } else {
           console.log(response, "error?");
         }
@@ -122,51 +166,85 @@ function App() {
   };
   return (
     <div className="App">
-      <BookedDates bookedDates={savedEvents} />
-      <EventInput
-        events={events}
-        eventDataErrors={eventDataErrors}
-        onChange={handleInputChange}
-        placeholder={"Summary"}
-        name={"summary"}
-        phrase={"summary"}
-        label={
-          "Which type of Booking is this? (e.g. 'Multi-night stay', 'Single-night stay')"
-        }
-      />
+      {savedEvents &&
+        savedEvents.map((event, idx) => {
+          return (
+            <Accordion placeholder={"stuff"}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                placeholder={"Booked dates"}
+              >
+                <h6>{`Booked Dates: ${bookedDates.start} - ${bookedDates.end} `}</h6>
+              </AccordionSummary>
+              <AccordionDetails
+                children={<BookedDates bookedDates={event} />}
+              ></AccordionDetails>
+            </Accordion>
+          );
+        })}
+      <Grid
+        container
+        style={{ paddingTop: "10%" }}
+        direction="column"
+        spacing={3}
+      >
+        <EventInput
+          events={events}
+          eventDataErrors={eventDataErrors}
+          onChange={handleInputChange}
+          placeholder={"Summary"}
+          name={"summary"}
+          phrase={"summary"}
+          label={
+            "Which type of Booking is this? (e.g. 'Multi-night stay', 'Single-night stay')"
+          }
+        />
+        <br />
+        <Grid item container direction="row">
+          <Grid item xs={6}>
+            <EventInput
+              events={events}
+              eventDataErrors={eventDataErrors}
+              onChange={handleInputChange}
+              placeholder={"startDate"}
+              name={"startDate"}
+              phrase={"startDate"}
+              label={"Start Date"}
+              type={"date"}
+            />
+          </Grid>
+          <br />
+          <Grid item xs={6}>
+            <EventInput
+              events={events}
+              eventDataErrors={eventDataErrors}
+              onChange={handleInputChange}
+              placeholder={"endDate"}
+              name={"endDate"}
+              phrase={"endDate"}
+              label={"End Date"}
+              type={"date"}
+            />
+          </Grid>
+        </Grid>
+        <br />
+        <EventInput
+          events={events}
+          eventDataErrors={eventDataErrors}
+          onChange={handleInputChange}
+          placeholder={"Number of Guests"}
+          name={"attendees"}
+          phrase={"attendees"}
+          label={"Number of Guests"}
+        />
+      </Grid>
       <br />
-      <EventInput
-        events={events}
-        eventDataErrors={eventDataErrors}
-        onChange={handleInputChange}
-        placeholder={"startDate"}
-        name={"startDate"}
-        phrase={"startDate"}
-        label={"startDate"}
-        type={"date"}
-      />
-      <br />
-      <EventInput
-        events={events}
-        eventDataErrors={eventDataErrors}
-        onChange={handleInputChange}
-        placeholder={"endDate"}
-        name={"endDate"}
-        phrase={"endDate"}
-        label={"endDate"}
-        type={"date"}
-      />
-      <br />
-      <EventInput
-        events={events}
-        eventDataErrors={eventDataErrors}
-        onChange={handleInputChange}
-        placeholder={"Number of Guests"}
-        name={"attendees"}
-        phrase={"attendees"}
-        label={"Number of Guests"}
-      />
-      <button onClick={handleAddEvent}>Add Event</button>
+      {/*<Button className={"button"} onClick={handleAddEvent}>*/}
+      {/*  Add Event*/}
+      {/*</Button>*/}
+      <StyledButton onClick={handleAddEvent}>Add Event</StyledButton>
       <br />
     </div>
   );
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {google} = require("googleapis");
-const {urlencoded} = require("express");
+const { google } = require("googleapis");
+const { urlencoded } = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 
-require("dotenv").config({path: "./.env"});
+require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
@@ -13,101 +13,99 @@ const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
 const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
 const GOOGLE_PROJECT_NUMBER = process.env.GOOGLE_PROJECT_NUMBER;
 const GOOGLE_CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
-const GOOGLE_CALENDAR_KEY_FILE = process.env.GOOGLE_CALENDAR_KEY_FILE;
 
 const jwtClient = new google.auth.JWT(
-    GOOGLE_CLIENT_EMAIL,
-    null,
-    GOOGLE_PRIVATE_KEY,
-    SCOPES
+  GOOGLE_CLIENT_EMAIL,
+  null,
+  GOOGLE_PRIVATE_KEY,
+  SCOPES
 );
 app.use(
-    cors({
-        origin: "*",
-        credentials: true,
-    })
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const calendar = google.calendar({
-    version: "v3",
-    project: GOOGLE_PROJECT_NUMBER,
-    auth: jwtClient,
+  version: "v3",
+  project: GOOGLE_PROJECT_NUMBER,
+  auth: jwtClient,
 });
 
 app.get("/", (req, res) => {
-    calendar.events.list(
-        {
-            calendarId: GOOGLE_CALENDAR_ID,
-            timeMin: new Date().toISOString(),
-            maxResults: 10,
-            singleEvents: true,
-            orderBy: "startTime",
-        },
-        (error, result) => {
-            if (error) {
-                res.send(JSON.stringify({error: error}));
-            } else {
-                if (result.data.items.length) {
-                    res.send(JSON.stringify({events: result.data.items}));
-                } else {
-                    res.send(JSON.stringify({message: "No upcoming events found."}));
-                }
-            }
+  calendar.events.list(
+    {
+      calendarId: GOOGLE_CALENDAR_ID,
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime",
+    },
+    (error, result) => {
+      if (error) {
+        res.send(JSON.stringify({ error: error }));
+      } else {
+        if (result.data.items.length) {
+          res.send(JSON.stringify({ events: result.data.items }));
+        } else {
+          res.send(JSON.stringify({ message: "No upcoming events found." }));
         }
-    );
+      }
+    }
+  );
 });
 app.post("/createEvent", cors(), (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    let newEvent = {
-        summary: req.body.summary,
-        location: "Pigeon Lake, AB, Canada",
-        description: "New Booking",
-        start: {
-            date: req.body.startDate,
-            timeZone: "UTC-7",
-        },
-        end: {
-            date: req.body.endDate,
-            timeZone: "UTC-7",
-        },
-        attendees: req.body.attendees,
-        reminders: {
-            useDefault: false,
-            overrides: [
-                {method: "email", minutes: 24 * 60},
-                {method: "popup", minutes: 10},
-            ],
-        },
-    };
-    console.log(newEvent, 'newEvent');
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "./bnb-calendar-access-a2a8476289eb.json",
-        scopes: "https://www.googleapis.com/auth/calendar",
-    });
-    auth.getClient().then((a) => {
-        calendar.events.insert(
-            {
-                auth: a,
-                calendarId: GOOGLE_CALENDAR_ID,
-                resource: newEvent,
-            },
-            function (err, event) {
-                if (err) {
-                    console.log(
-                        "There was an error contacting the Calendar service: " +
-                        err.errors
-                    );
-                    console.log(err.errors);
-                    return;
-                }
-                console.log("Event created: %s", event.data);
-                res.jsonp(`Event successfully created! /n ${event.data}`);
-            }
-        );
-    });
+  res.setHeader("Content-Type", "application/json");
+  let newEvent = {
+    summary: req.body.summary,
+    location: "Pigeon Lake, AB, Canada",
+    description: "New Booking",
+    start: {
+      date: req.body.startDate,
+      timeZone: "UTC-7",
+    },
+    end: {
+      date: req.body.endDate,
+      timeZone: "UTC-7",
+    },
+    attendees: req.body.attendees,
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: "email", minutes: 24 * 60 },
+        { method: "popup", minutes: 10 },
+      ],
+    },
+  };
+  console.log(newEvent, "newEvent");
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "./bnb-calendar-access-a2a8476289eb.json",
+    scopes: "https://www.googleapis.com/auth/calendar",
+  });
+  auth.getClient().then((a) => {
+    calendar.events.insert(
+      {
+        auth: a,
+        calendarId: GOOGLE_CALENDAR_ID,
+        resource: newEvent,
+      },
+      function (err, event) {
+        if (err) {
+          console.log(
+            "There was an error contacting the Calendar service: " + err.errors
+          );
+          console.log(err.errors);
+          return;
+        }
+        console.log("Event created: %s", event.data);
+        res.jsonp(`Event successfully created! /n ${event.data}`);
+      }
+    );
+  });
 });
 
 app.listen(3000, () => console.log(`App listening on port 3000!`));
