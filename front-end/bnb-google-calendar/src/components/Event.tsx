582bb2865fedd9030d08e421659ae989b0c09276
Bryce Young <byoung006@gmail.com>
2022-12-18 21:55:55 -0800

    Typed the id cominginto the Event FC, removed a console.

--- front-end/bnb-google-calendar/src/components/Event.tsx
+++ front-end/bnb-google-calendar/src/components/Event.tsx
@@ -3,41 +3,29 @@ import axios from "axios";
 import getBackendApi from "../helper-functions/getBackendApi.jsx";
 import { TextField } from "@mui/material";
 import { SingleEvent } from "../interfaces/PromiseInterfaces";
-type getEvent = () => Promise<SingleEvent>;
-const Event: React.FC<SingleEvent> = ({ eventId }) => {
+const Event: React.FC<SingleEvent> = ({ eventId }:SingleEvent) => {
   const [event, setEvent] = useState([]);
+  console.log(event, "event summary")
   const usefulDetails = ["summary", "description", "start", "end", "htmlLink"];
-  const getSingleEvent = async () => {
+  async function getSingleEvent() {
     const { data } = await axios.get(
       `${getBackendApi()}/event?eventId=${eventId}`
     );
-    // console.log(data, "data");
     setEvent(data.event);
   };
-
   useEffect(() => {
     getSingleEvent();
   }, []);
+
   return (
     <>
       <div style={{ padding: "5% 0 5% 0" }}>
-        {Object.entries(event).map((value, index) => {
-          let usefulEventDetails = value.reduce((acc, curr) => {
-            console.log(curr, "curr");
-            if (usefulDetails.includes(curr)) {
-              if (typeof curr[0] === "object") {
-                let cats = Object.entries(value[1]);
-                console.log(cats, "cats");
-                acc.push(cats);
-              }
-              acc.push(curr);
-            }
-            console.log(acc, "acc");
-            return acc;
-          });
-          console.log(usefulEventDetails, "usefulEventDetails");
-          return <TextField multiline value={value} />;
-        })}
+        {usefulDetails.map((e)=>{ if (typeof e === 'object')
+        {return Object.entries(e)} 
+          return (
+            <>{e}</>
+          )} 
+        )}
       </div>
       <br />
     </>
