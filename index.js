c878d53b42d5190515adb0e51ad181a1328bd0c4
Bryce Young <byoung006@gmail.com>
2022-12-20 09:50:26 -0800

    I think this is correct now?

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const logger = require("morgan");
 
 require("dotenv").config({ path: "./.env" });
-//hello there this is a test
+//hello there this is a test and I think we got it
 const app = express();
 const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
